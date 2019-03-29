85212f56e6d21dcc318f7518536e4e6e69139c32
LolaMelibaeva <lolam79@gmail.com>
2019-03-28 22:06:39 -0400

    Class tasks

---
+++ src/com/class13/ClassTask.java
@@ -0,0 +1,14 @@
+package com.class13;
+
+public class ClassTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="Sunday";
+		for (int i=str.length()-1; i>=0; i--) {
+			System.out.print((str.charAt(i)));
+		}
+	}
+
+}
---
+++ src/com/class13/ClassTask1.java
@@ -0,0 +1,19 @@
+package com.class13;
+
+public class ClassTask1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="Wednesday";
+		boolean str1=str.isEmpty();
+		System.out.println(str1);
+		System.out.println();
+		
+		if ((str.length()%2!=0) && (str.length()>=3)){
+	
+		System.out.println(str.charAt(str.length()/2));
+		
+		}
+}
+}
